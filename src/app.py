5af068129e45f9c91acd1afa37427c809d48c385
archds <dsalekseev11@gmail.com>
2024-08-13 16:54:57 +0700

    Add basic template

--- src/app.py
+++ src/app.py
@@ -1,17 +1,33 @@
+from pathlib import Path
 import litestar
 from litestar.config.csrf import CSRFConfig
 from litestar.config.compression import CompressionConfig
 from litestar.template.config import TemplateConfig
+from litestar.response import Template
+from litestar.contrib.htmx.response import HTMXTemplate
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.logging import LoggingConfig
+
+CWD = Path(__file__).parent
 
 
 @litestar.get("/")
-async def index() -> str:
-    return "Hello, world!"
+async def index() -> Template:
+    return HTMXTemplate(template_name="index.html", context={})
 
 
 app = litestar.Litestar(
     route_handlers=[index],
     csrf_config=CSRFConfig(secret="test-secret"),
     compression_config=CompressionConfig(backend="gzip"),
-    template_config=TemplateConfig(directory="templates", engine="jinja2"),
+    template_config=TemplateConfig(directory=CWD / "templates", engine=JinjaTemplateEngine),
+    logging_config=LoggingConfig(
+        root={"level": "INFO", "handlers": ["queue_listener"]},
+        formatters={
+            "standard": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            }
+        },
+        log_exceptions="debug",
+    ),
 )
