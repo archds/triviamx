97966e491df076e2100e74cc64e3f607bba2adab
archds <dsalekseev11@gmail.com>
2024-08-14 16:05:22 +0700

    Game session players

--- src/app.py
+++ src/app.py
@@ -1,18 +1,22 @@
 import asyncio
 import datetime
+import random
+import uuid
 import litestar
 from litestar.config.csrf import CSRFConfig
 from litestar.config.compression import CompressionConfig
 import litestar.di
+import litestar.status_codes
 from litestar.template.config import TemplateConfig
 from litestar.response import Template
-from litestar.contrib.htmx.response import HTMXTemplate, HXLocation
+from litestar.contrib.htmx.response import HTMXTemplate, HXLocation, ClientRedirect
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.logging import LoggingConfig
 from litestar.middleware.session.server_side import ServerSideSessionConfig
 from litestar.stores.file import FileStore
 from litestar.contrib.htmx.request import HTMXRequest
 from litestar.static_files import create_static_files_router
+from litestar.response import Redirect
 import pydantic
 import state
 from api import OpenTriviaDB, get_open_trivia_db
@@ -23,7 +27,7 @@ session_config = ServerSideSessionConfig()
 
 
 class ClientSessionData(pydantic.BaseModel):
-    session_id: str
+    player_session_id: str
     game_session: state.GameSession
 
 
@@ -31,32 +35,38 @@ def on_startup():
     config.ASSETS_DIR.mkdir(exist_ok=True)
 
 
-@litestar.get("/")
+@litestar.get("/", status_code=litestar.status_codes.HTTP_302_FOUND)
 async def index(
     request: litestar.Request,
     session_manager: state.GameSessionManager,
-) -> Template:
+) -> Redirect:
     session_id = request.get_session_id()
     assert session_id
     game_session = await session_manager.open_session(session_id)
-    data = ClientSessionData(session_id=session_id, game_session=game_session)
+
+    return Redirect(path=f"/game/{game_session.id}")
+
+
+@litestar.get("/game/{game_session_id:str}")
+async def room(
+    request: litestar.Request,
+    game_session_id: uuid.UUID,
+    session_manager: state.GameSessionManager,
+) -> Template:
+    player_session_id = request.get_session_id()
+    assert player_session_id
+    game_session = await session_manager.get_session(game_session_id)
+    data = ClientSessionData(
+        player_session_id=player_session_id,
+        game_session=game_session,
+    )
+
+    request.set_session(data.model_dump())
 
     return HTMXTemplate(
         template_name="index.html",
         context=data.model_dump(),
         push_url=f"/{game_session.id}",
-    )
-
-
-@litestar.get("/answers")
-async def answers(request: HTMXRequest, guess: str) -> Template:
-    session = state.GameState.model_validate(request.session)
-    session.question.guess = guess
-
-    return HTMXTemplate(
-        template_name="answers.html",
-        context=session.model_dump(),
-        re_swap="outerHTML",
     )
 
 
@@ -74,17 +84,30 @@ async def redirect_to(
     return HXLocation(redirect_to=to, swap="outerHTML")
 
 
-@litestar.websocket_listener("/gameroom")
-async def gameroom(data: dict[str, str], socket: litestar.WebSocket) -> None:
-    pass
+@litestar.websocket_listener("/ws")
+async def ws(
+    data: dict[str, str],
+    socket: litestar.WebSocket,
+    game_session_id: uuid.UUID,
+    session_manager: state.GameSessionManager,
+) -> HTMXTemplate:
+    player_session_id = socket.get_session_id()
+    assert player_session_id
+    game_session = await session_manager.get_session(game_session_id)
+    ctx = ClientSessionData(
+        player_session_id=player_session_id,
+        game_session=game_session,
+    )
+
+    return HTMXTemplate(template_name="answers.html", context=ctx)
 
 
 app = litestar.Litestar(
     route_handlers=[
         index,
-        answers,
         redirect_to,
-        gameroom,
+        ws,
+        room,
         create_static_files_router(path="/static", directories=[config.ASSETS_DIR]),
     ],
     csrf_config=CSRFConfig(secret="test-secret"),
